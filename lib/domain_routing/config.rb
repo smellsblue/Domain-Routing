652ed204608e964426df587ec99927e3c10a4b19
Mike Stone <stonemj@gmail.com>
2011-08-22 22:09:20 -0700

    Downcase domains and subdomains... otherwise if the browser sends mixed case domains, there can be issues if you expect them to be always downcased.  And some browsers seem to proactively downcase for you, so case in a domain doesn't seem to make sense.

--- lib/domain_routing/config.rb
+++ lib/domain_routing/config.rb
@@ -10,11 +10,11 @@ module DomainRouting
       end
 
       def main_domains=(*values)
-        @main_domains = values.flatten
+        @main_domains = values.flatten.map(&:downcase)
       end
 
       def invalid_subdomains=(*values)
-        @invalid_subdomains = values.flatten
+        @invalid_subdomains = values.flatten.map(&:downcase)
       end
     end
   end
--- lib/domain_routing/util.rb
+++ lib/domain_routing/util.rb
@@ -2,13 +2,14 @@ module DomainRouting
   class Util
     class << self
       def domain_for(request)
-        return "" if DomainRouting::Config.main_domains.include?(request.domain)
-        request.domain
+        domain = request.domain.downcase
+        return "" if DomainRouting::Config.main_domains.include?(domain)
+        domain
       end
 
       def subdomain_for(request)
         return "" unless request.subdomains.present?
-        subdomain = request.subdomains.last
+        subdomain = request.subdomains.last.downcase
         return "" if DomainRouting::Config.invalid_subdomains.include?(subdomain)
         subdomain
       end
